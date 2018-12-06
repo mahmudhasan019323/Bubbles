e397bb8c6649a20612c0cd6147ab0be315e6d32a
mahmud <mahmudhasan019323@gmail.com>
2018-12-07 01:36:12 +0600

    added touch/click feature

--- js/paperscript.js
+++ js/paperscript.js
@@ -1,27 +1,36 @@
 var circles = [];
+var keys = 'qwertyuiopasdfghjklzxcvbnm';
 
 function onKeyDown(e){
-    if(keyData[e.key]){
-      var maxPoint = new Point(view.size.width, view.size.height);
-      var randomPoint = Point.random();
-      var point = maxPoint * randomPoint;
-      var radius = 300;
-      var newCircle = new Path.Circle(point, radius);
-      keyData[e.key].sound.play();
-      newCircle.fillColor = keyData[e.key].color;
-      circles.push(newCircle);
-      console.log(circles);
-    }
-  }
+    test(e.key);
+}
 
-  function onFrame(e){
+function onMouseDown(){
+  var key = keys[Math.round(Math.random() * keys.length)];
+  test(key);
+}
+
+function test(key){
+  if(keyData[key]){
+    var maxPoint = new Point(view.size.width, view.size.height);
+    var randomPoint = Point.random();
+    var point = maxPoint * randomPoint;
+    var radius = 300;
+    var newCircle = new Path.Circle(point, radius);
+    keyData[key].sound.play();
+    newCircle.fillColor = keyData[key].color;
+    circles.push(newCircle);
+    console.log(circles);
+  }
+}
+
+  function onFrame(){
       circles.forEach(function(circle,index){
       circle.fillColor.hue++;
       circle.scale(.94);
       if(circle.area < 1){
         circle.remove();
         circles.splice(index,1);
-        console.log(circles);
       }
 
     });
